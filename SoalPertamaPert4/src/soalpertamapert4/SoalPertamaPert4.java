d6ee3f65db3ef99aedb9fe1304ebc7074753d0fe
Natasya Destiani <145103195+natasyadestiani@users.noreply.github.com>
2024-02-26 21:06:09 +0700

    Add files via upload

---
+++ SoalPertamaPert4/src/soalpertamapert4/SoalPertamaPert4.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package soalpertamapert4;
+
+/**
+ *
+ * @author ASUS
+ */
+import java.util.Scanner;
+
+public class SoalPertamaPert4 {
+
+    public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Input n: ");
+            int n = input.nextInt();
+
+            for (int i = 1; i <= n; i++) {
+                System.out.println(i);
+            }
+    }
+}
+
